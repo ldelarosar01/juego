ead879e29b69607094d9c1165412a45e9dfe6585
Luis <luisdelarosaruiz@gmail.com>
2016-10-18 10:16:32 +0200

    añadiendo todos los directorios

--- principal.py
+++ principal.py
@@ -1,4 +1,3 @@
 import pygame 
-while True:
-    pygame.display.set_mode((320, 240))
-    logotipo = pygame.image.load("./images/fondo.png")
+pygame.display.set_mode((640, 480))
+logotipo = pygame.image.load("./images/fondo.png")
