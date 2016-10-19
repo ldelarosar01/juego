2911464d97efc019ccca9bf3b9873b8ca1ab700a
Luis <luisdelarosaruiz@gmail.com>
2016-10-19 18:28:05 +0200

    Configuracion de la estructura del programa , primer cambio realizado, añadido un fondo blanco para poner sprites

--- principal.py
+++ principal.py
@@ -1,3 +1,41 @@
-import pygame 
-pygame.display.set_mode((640, 480))
-logotipo = pygame.image.load("./images/fondo.png")
+
+
+import sys, pygame
+from pygame.locals import *
+ 
+
+WIDTH = 640
+HEIGHT = 480
+ 
+
+ 
+def load_image(filename, transparent=False):
+        try: image = pygame.image.load(filename)
+        except pygame.error, message:
+                raise SystemExit, message
+        image = image.convert()
+        if transparent:
+                color = image.get_at((0,0))
+                image.set_colorkey(color, RLEACCEL)
+        return image
+ 
+# ---------------------------------------------------------------------
+ 
+def main():
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("Pruebas Pygame")
+ 
+    background_image = load_image('images/fondo.jpg')
+ 
+    while True:
+        for eventos in pygame.event.get():
+            if eventos.type == QUIT:
+                sys.exit(0)
+ 
+        screen.blit(background_image, (0, 0))
+        pygame.display.flip()
+    return 0
+ 
+if __name__ == '__main__':
+    pygame.init()
+    main()
