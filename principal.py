c82914988cb8fd0ee32d17b7769a362c1a30c317
Luis <luisdelarosaruiz@gmail.com>
2016-10-19 18:50:49 +0200

    Añadiendo mi primer sprite un dragoncito plagiado del de google

--- principal.py
+++ principal.py
@@ -2,11 +2,19 @@
 
 import sys, pygame
 from pygame.locals import *
+from Tkconstants import TOP
  
 
 WIDTH = 640
 HEIGHT = 480
- 
+class Dragon(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = load_image("images/dragon.png", True)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH / 2
+        self.rect.centery = HEIGHT / 2
+        self.speed = [0.5, -0.5]
 
  
 def load_image(filename, transparent=False):
@@ -26,13 +34,14 @@ def main():
     pygame.display.set_caption("Pruebas Pygame")
  
     background_image = load_image('images/fondo.jpg')
- 
+    dragon = Dragon();
     while True:
         for eventos in pygame.event.get():
             if eventos.type == QUIT:
                 sys.exit(0)
  
         screen.blit(background_image, (0, 0))
+        screen.blit(dragon.image, dragon.rect)
         pygame.display.flip()
     return 0
  
