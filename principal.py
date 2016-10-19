9a2b8fd4cf18f413db86c1434f08a2bc70b45754
Luis <luisdelarosaruiz@gmail.com>
2016-10-19 19:48:42 +0200

    añdido fondo sin completar de donde va a estar el dragoncito

--- principal.py
+++ principal.py
@@ -7,15 +7,24 @@ from Tkconstants import TOP
 
 WIDTH = 640
 HEIGHT = 480
+
 class Dragon(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = load_image("images/dragon.png", True)
+        self.image = load_image("images/dragon2.png", True)
         self.rect = self.image.get_rect()
         self.rect.centerx = WIDTH / 2
         self.rect.centery = HEIGHT / 2
         self.speed = [0.5, -0.5]
-
+         
+    def actualizar(self, time):
+        self.rect.centerx += self.speed[0] * time
+        if self.rect.left <= 0 or self.rect.right >= WIDTH:
+            self.speed[0] = +self.speed[0]
+            self.rect.centerx += self.speed[0] * time
+        if self.rect.right <= 0 or self.rect.bottom >= HEIGHT:
+            self.speed[1] = +self.speed[0]
+            self.rect.centery += self.speed[0] * time
  
 def load_image(filename, transparent=False):
         try: image = pygame.image.load(filename)
@@ -35,11 +44,17 @@ def main():
  
     background_image = load_image('images/fondo.jpg')
     dragon = Dragon();
+    
+    clock = pygame.time.Clock()
+   
     while True:
+       
         for eventos in pygame.event.get():
+            
             if eventos.type == QUIT:
                 sys.exit(0)
- 
+        time = clock.tick(600)
+        dragon.actualizar(time)
         screen.blit(background_image, (0, 0))
         screen.blit(dragon.image, dragon.rect)
         pygame.display.flip()
